b1d76d68bfcb61f2fe368e9d2832bce99c47f4b8
sturms_robot <151828922+sturmsrobot@users.noreply.github.com>
2024-08-26 15:58:09 +0200

    Implemented useState and useEffect

--- 01-Frontend/meine-react-app/src/App.js
+++ 01-Frontend/meine-react-app/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Du hast ${count} Mal geklickt!`;
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Du hast {count} Mal geklickt!</p>
+      <button onClick={() => setCount(count + 1)}>Klick mich!</button>
     </div>
   );
 }
